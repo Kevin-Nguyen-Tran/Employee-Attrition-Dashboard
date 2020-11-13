c5193edcad5e64af20c965dcfa14226c37d7c356
Kevin Tran <k.n.tran898@gmail.com>
2020-11-12 20:20:00 -0500

    Utilized ggplot more for the functions. Felt more natural. Will use moving forward. Added colors to the plots.

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 library(shiny)
 library(tidyverse)
+library(wesanderson)
+
 data <- read.csv("C:/Users/Kevin/Desktop/Employee-Attrition-Dashboard/WA_Fn-UseC_-HR-Employee-Attrition.csv")
 employee_data <- as.tibble(data)
 
@@ -11,19 +13,27 @@ employee_data2 <- employee_data %>%
 
 employee_data3 <- top_n(employee_data, 50)
 
-#ui <- fluidPage(
-  #selectInput(inputId = "varix", label = "Choose X Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
-  #selectInput(inputId = "variy", label = "Choose Y Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
- # plotOutput(outputId = "scatter")
-#)
+#--------------------------------------------------------------------------------------------------------------------------------
 
-#server <- function(input, output){
- # output$scatter <- renderPlot({ggplot(data = employee_data, aes(x = input$varix, y = input$variy)) +
-  #    geom_point()})
+#--------------------------------------------------------------------------------------------------------------------------------
+
+ui <- fluidPage(
+  selectInput(inputId = "varix", label = "Choose X Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
+  selectInput(inputId = "variy", label = "Choose Y Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
+  plotOutput(outputId = "scatter")
+)
+
+server <- function(input, output){
+  output$scatter <- renderPlot({ggplot(data = employee_data, aes(x = input$varix, y = input$variy)) +
+      geom_point()})
   
-#}
+}
 
-#shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
+
+#--------------------------------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------------------------------------------------------
 
 ui <- fluidPage(
     radioButtons(inputId = "plotType", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
@@ -32,7 +42,26 @@ ui <- fluidPage(
 
 server <- function(input, output){
   output$plot <- renderPlot({
-    plot(employee_data2, type = input$plotType)
+    if(input$plotType == "p"){
+      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+        geom_point() +
+        geom_smooth(se = FALSE) +
+        labs(x = "Age", 
+             y = "Hourly Rate",
+             title = "Is Salary Affected by Age?",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    } else {
+      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+        geom_line() +
+        labs(x = "Age", 
+             y = "Hourly Rate",
+             title = "Is Salary Affected by Age?",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    }
   })
   
 }
@@ -65,9 +94,40 @@ server <- function(input, output){
 
 shinyApp(ui = ui, server = server)
 
+#--------------------------------------------------------------------------------------------------------------------------------
 
+#--------------------------------------------------------------------------------------------------------------------------------
 
+ui <- fluidPage(
+  radioButtons(inputId = "plotType", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
+  plotOutput(outputId = "plot")
+)
 
+server <- function(input, output){
+  output$plot <- renderPlot({
+    if(input$plotType == "box"){
+      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+        geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
+        labs(x = "Business Travel", 
+             y = "Job Satisfaction",
+             title = "Satisfaction Level in Regards to Travel",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    } else {
+      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+        geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
+        labs(x = "Business Travel", 
+             y = "Job Satisfaction",
+             title = "Satisfaction Level in Regards to Travel",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
 
 
 

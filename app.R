4d79d1c8acaed36e9c8f04ecbc7a027fd54777c3
Kevin Tran <k.n.tran898@gmail.com>
2020-11-13 21:57:42 -0500

    Combined all 3 shiny applications into one using Navbar! Very happy all worked together. Will add stats to each tab to add tabular info.

--- app.R
+++ app.R
@@ -14,25 +14,25 @@ employee_data2 <- employee_data %>%
 employee_data3 <- top_n(employee_data, 50)
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
+#DIDN'T WORK WILL NOT USE IN NAVBAR
 #--------------------------------------------------------------------------------------------------------------------------------
 
-ui <- fluidPage(
-  selectInput(inputId = "varix", label = "Choose X Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
-  selectInput(inputId = "variy", label = "Choose Y Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
-  plotOutput(outputId = "scatter")
-)
+#ui <- fluidPage(
+ # selectInput(inputId = "varix", label = "Choose X Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
+  #selectInput(inputId = "variy", label = "Choose Y Variable", "Distance" = "employee_data[ ,4]", "Age" = "employee_data[ ,1]"),
+  #plotOutput(outputId = "scatter")
+#)
 
-server <- function(input, output){
-  output$scatter <- renderPlot({ggplot(data = employee_data, aes(x = input$varix, y = input$variy)) +
-      geom_point()})
+#server <- function(input, output){
+ # output$scatter <- renderPlot({ggplot(data = employee_data, aes(x = input$varix, y = input$variy)) +
+  #    geom_point()})
   
-}
+#}
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
+# Age vs hourly rate
 #--------------------------------------------------------------------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -70,7 +70,7 @@ shinyApp(ui = ui, server = server)
 
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
+# Age data
 #--------------------------------------------------------------------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -95,7 +95,7 @@ server <- function(input, output){
 shinyApp(ui = ui, server = server)
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
+# Business Travel vs Job Satisfaction
 #--------------------------------------------------------------------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -129,11 +129,82 @@ server <- function(input, output){
 
 shinyApp(ui = ui, server = server)
 
+#--------------------------------------------------------------------------------------------------------------------------------
+#NAVBAR FOR THE ABOVE 3 SHINY APPS
+#--------------------------------------------------------------------------------------------------------------------------------
 
+ui <- navbarPage(title = "Employee Attrition EDA",
+           tabPanel("Age Data",
+                    radioButtons(inputId = "plotType1", label = "Plot Type:", choices = c("Box Plot" = "box", "Bar Chart" = "bar")),
+                    plotOutput(outputId = "plot1")
+                    ),
+           tabPanel("Age vs Hourly Rate",
+                    radioButtons(inputId = "plotType2", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
+                    plotOutput(outputId = "plot2")
+                    ),
+           tabPanel("Business Travel and Job Satisfaction",
+                    radioButtons(inputId = "plotType3", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
+                    plotOutput(outputId = "plot3")
+                    )
+           )
 
+server <- function(input, output){
+  output$plot1 <- renderPlot({
+    if(input$plotType1 == "box"){
+      boxplot(employee_data3$Age,
+              xlab = "Employees",
+              ylab = "Age (averages")
+    } else {
+      barplot(employee_data3$Age,
+              xlab = "Employee",
+              ylab = "Age")
+    }
+  })
+  output$plot2 <- renderPlot({
+    if(input$plotType2 == "p"){
+      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+        geom_point() +
+        geom_smooth(se = FALSE) +
+        labs(x = "Age", 
+             y = "Hourly Rate",
+             title = "Is Salary Affected by Age?",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    } else {
+      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+        geom_line() +
+        labs(x = "Age", 
+             y = "Hourly Rate",
+             title = "Is Salary Affected by Age?",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    }
+  })
+  output$plot3 <- renderPlot({
+    if(input$plotType3 == "box"){
+      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+        geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
+        labs(x = "Business Travel", 
+             y = "Job Satisfaction",
+             title = "Satisfaction Level in Regards to Travel",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    } else {
+      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+        geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
+        labs(x = "Business Travel", 
+             y = "Job Satisfaction",
+             title = "Satisfaction Level in Regards to Travel",
+             caption = "Source: IBM HR Analytics") +
+        theme(plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5))
+    }
+  })
+}
 
-
-
-
+shinyApp(ui = ui, server = server)
 
 

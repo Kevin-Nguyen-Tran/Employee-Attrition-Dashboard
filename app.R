56105f7ecd610fae8bc1f5c455f998829074523a
Kevin Tran <k.n.tran898@gmail.com>
2020-11-16 20:05:27 -0500

    Having trouble with publish.

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(wesanderson)
 library(shinydashboard)
 
-data <- read.csv("C:/Users/Kevin/Desktop/Employee-Attrition-Dashboard/WA_Fn-UseC_-HR-Employee-Attrition.csv")
+data <- read.csv("C:/Users/Kevin/Desktop/data/HR-Employee-Attrition-Dashboard.csv")
 employee_data <- as.tibble(data)
 
 employee_data <- rename(employee_data, Age = ï..Age)
@@ -39,218 +39,218 @@ raw_data4 <- employee_data3 %>%
 # Age vs hourly rate
 #--------------------------------------------------------------------------------------------------------------------------------
 
-ui <- fluidPage(
-    radioButtons(inputId = "plotType", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
-    plotOutput(outputId = "plot")
-  )  
+#ui <- fluidPage(
+ #   radioButtons(inputId = "plotType", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
+  #  plotOutput(outputId = "plot")
+  #)  
 
-server <- function(input, output){
-  output$plot <- renderPlot({
-    if(input$plotType == "p"){
-      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
-        geom_point() +
-        geom_smooth(se = FALSE) +
-        labs(x = "Age", 
-             y = "Hourly Rate",
-             title = "Is Salary Affected by Age?",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    } else {
-      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
-        geom_line() +
-        labs(x = "Age", 
-             y = "Hourly Rate",
-             title = "Is Salary Affected by Age?",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    }
-  })
+#server <- function(input, output){
+ # output$plot <- renderPlot({
+  #  if(input$plotType == "p"){
+   #   ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+    #    geom_point() +
+     #   geom_smooth(se = FALSE) +
+      #  labs(x = "Age", 
+       #      y = "Hourly Rate",
+        #     title = "Is Salary Affected by Age?",
+         #    caption = "Source: IBM HR Analytics") +
+      #  theme(plot.title = element_text(hjust = 0.5),
+       #       plot.subtitle = element_text(hjust = 0.5))
+  #  } else {
+   #   ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+    #    geom_line() +
+     #   labs(x = "Age", 
+      #       y = "Hourly Rate",
+       #      title = "Is Salary Affected by Age?",
+        #     caption = "Source: IBM HR Analytics") +
+        #theme(plot.title = element_text(hjust = 0.5),
+         #     plot.subtitle = element_text(hjust = 0.5))
+    #}
+  #})
   
-}
+#}
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # Age data
 #--------------------------------------------------------------------------------------------------------------------------------
 
-ui <- fluidPage(
-  radioButtons(inputId = "plotType", label = "Plot Type:", choices = c("Box Plot" = "box", "Bar Chart" = "bar")),
-  plotOutput(outputId = "plot")
-)
+#ui <- fluidPage(
+ # radioButtons(inputId = "plotType", label = "Plot Type:", choices = c("Box Plot" = "box", "Bar Chart" = "bar")),
+  #plotOutput(outputId = "plot")
+#)
 
-server <- function(input, output){
-  output$plot <- renderPlot({
-    if(input$plotType == "box"){
-      boxplot(employee_data3$Age,
-              xlab = "Employees",
-              ylab = "Age (averages")
-    } else {
-      barplot(employee_data3$Age,
-              xlab = "Employee",
-              ylab = "Age")
-    }
-  })
-}
+#server <- function(input, output){
+ # output$plot <- renderPlot({
+  #  if(input$plotType == "box"){
+   #   boxplot(employee_data3$Age,
+    #          xlab = "Employees",
+     #         ylab = "Age (averages")
+    #} else {
+     # barplot(employee_data3$Age,
+      #        xlab = "Employee",
+       #       ylab = "Age")
+    #}
+  #})
+#}
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # Business Travel vs Job Satisfaction
 #--------------------------------------------------------------------------------------------------------------------------------
 
-ui <- fluidPage(
-  radioButtons(inputId = "plotType", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
-  plotOutput(outputId = "plot")
-)
+#ui <- fluidPage(
+ # radioButtons(inputId = "plotType", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
+ # plotOutput(outputId = "plot")
+#)
 
-server <- function(input, output){
-  output$plot <- renderPlot({
-    if(input$plotType == "box"){
-      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
-        geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
-        labs(x = "Business Travel", 
-             y = "Job Satisfaction",
-             title = "Satisfaction Level in Regards to Travel",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    } else {
-      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
-        geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
-        labs(x = "Business Travel", 
-             y = "Job Satisfaction",
-             title = "Satisfaction Level in Regards to Travel",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    }
-  })
-}
+#server <- function(input, output){
+ # output$plot <- renderPlot({
+  #  if(input$plotType == "box"){
+   #   ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+     #   geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
+   #     labs(x = "Business Travel", 
+          #   y = "Job Satisfaction",
+        #     title = "Satisfaction Level in Regards to Travel",
+       #      caption = "Source: IBM HR Analytics") +
+   #     theme(plot.title = element_text(hjust = 0.5),
+       #       plot.subtitle = element_text(hjust = 0.5))
+   # } else {
+    #  ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+    #    geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
+     #   labs(x = "Business Travel", 
+        #     y = "Job Satisfaction",
+         #    title = "Satisfaction Level in Regards to Travel",
+        #     caption = "Source: IBM HR Analytics") +
+       # theme(plot.title = element_text(hjust = 0.5),
+             # plot.subtitle = element_text(hjust = 0.5))
+  #  }
+#  })
+#}
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 #--------------------------------------------------------------------------------------------------------------------------------
 #NAVBAR FOR THE ABOVE 3 SHINY APPS
 #--------------------------------------------------------------------------------------------------------------------------------
 
-ui <- navbarPage(title = "Employee Attrition EDA",
-           tabPanel("Age Data",
-                    radioButtons(inputId = "plotType1", label = "Plot Type:", choices = c("Box Plot" = "box", "Bar Chart" = "bar")),
-                    plotOutput(outputId = "plot1"),
-                    verbatimTextOutput(outputId = "sum1")
-                    ),
-           tabPanel("Age vs Hourly Rate",
-                    radioButtons(inputId = "plotType2", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
-                    plotOutput(outputId = "plot2"),
-                    tableOutput(outputId = "sum2")
-                    ),
-           tabPanel("Business Travel and Job Satisfaction",
-                    radioButtons(inputId = "plotType3", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
-                    plotOutput(outputId = "plot3"),
-                    tableOutput(outputId = "sum3")
-                    )
-           )
+#ui <- navbarPage(title = "Employee Attrition EDA",
+           #tabPanel("Age Data",
+           #         radioButtons(inputId = "plotType1", label = "Plot Type:", choices = c("Box Plot" = "box", "Bar Chart" = "bar")),
+           #         plotOutput(outputId = "plot1"),
+            #        verbatimTextOutput(outputId = "sum1")
+          #          ),
+          # tabPanel("Age vs Hourly Rate",
+                   # radioButtons(inputId = "plotType2", label = "Plot Type:",choices = c("Scatter" = "p", "Line" = "l")),
+                  #  plotOutput(outputId = "plot2"),
+                 #   tableOutput(outputId = "sum2")
+                #    ),
+         #  tabPanel("Business Travel and Job Satisfaction",
+                #    radioButtons(inputId = "plotType3", label = "Plot Type:", choices = c("Box Plot" = "box", "Violin Plot" = "viola")),
+                #    plotOutput(outputId = "plot3"),
+                 #   tableOutput(outputId = "sum3")
+                 #   )
+         #  )
 
-server <- function(input, output){
-  output$plot1 <- renderPlot({
-    if(input$plotType1 == "box"){
-      boxplot(employee_data3$Age,
-              xlab = "Employees",
-              ylab = "Age (averages")
-    } else {
-      barplot(employee_data3$Age,
-              xlab = "Employee",
-              ylab = "Age")
-    }
-  })
-  output$sum1 <- renderPrint({
-    if(input$plotType1 == "box"){
-      summary(employee_data3$Age)
-    } else {
-      summary(employee_data3$Age)
-    }
-  })
+#server <- function(input, output){
+ # output$plot1 <- renderPlot({
+   # if(input$plotType1 == "box"){
+    #  boxplot(employee_data3$Age,
+             # xlab = "Employees",
+            #  ylab = "Age (averages")
+   # } else {
+     # barplot(employee_data3$Age,
+           #   xlab = "Employee",
+           #   ylab = "Age")
+  #  }
+ # })
+ # output$sum1 <- renderPrint({
+   # if(input$plotType1 == "box"){
+   #   summary(employee_data3$Age)
+  #  } else {
+  #    summary(employee_data3$Age)
+ #   }
+#  })
   #code above is for tab 1
   
-  output$plot2 <- renderPlot({
-    if(input$plotType2 == "p"){
-      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
-        geom_point() +
-        geom_smooth(se = FALSE) +
-        labs(x = "Age", 
-             y = "Hourly Rate",
-             title = "Is Salary Affected by Age?",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    } else {
-      ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
-        geom_line() +
-        labs(x = "Age", 
-             y = "Hourly Rate",
-             title = "Is Salary Affected by Age?",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    }
-  })
+ # output$plot2 <- renderPlot({
+    #if(input$plotType2 == "p"){
+     # ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+      #  geom_point() +
+       # geom_smooth(se = FALSE) +
+       # labs(x = "Age", 
+         #    y = "Hourly Rate",
+          #   title = "Is Salary Affected by Age?",
+          #   caption = "Source: IBM HR Analytics") +
+        #theme(plot.title = element_text(hjust = 0.5),
+             # plot.subtitle = element_text(hjust = 0.5))
+  #  } else {
+     # ggplot(employee_data3, aes(x = Age, y = HourlyRate)) +
+      #  geom_line() +
+     #   labs(x = "Age", 
+        #     y = "Hourly Rate",
+            # title = "Is Salary Affected by Age?",
+        #     caption = "Source: IBM HR Analytics") +
+       # theme(plot.title = element_text(hjust = 0.5),
+         #     plot.subtitle = element_text(hjust = 0.5))
+   # }
+ # })
   
-  output$sum2 <- renderTable({
-    if(input$plotType1 == "p"){
-      employee_data3 %>% 
-        select(Age, HourlyRate) %>% 
-        summarise(across(everything(), list(min, median, mean, max))) %>%
-        rename(Age.Min = Age_1, Age.Median = Age_2 , Age.Mean = Age_3 , Age.Max = Age_4, 
-               HourlyRate.Min = HourlyRate_1, HourlyRate.Median = HourlyRate_2, HourlyRate.Mean = HourlyRate_3, HourlyRate.Max = HourlyRate_4)
-    } else {
-      employee_data3 %>% 
-        select(Age, HourlyRate) %>% 
-        summarise(across(everything(), list(min, median, mean, max))) %>%
-        rename(Age.Min = Age_1, Age.Median = Age_2 , Age.Mean = Age_3 , Age.Max = Age_4, 
-               HourlyRate.Min = HourlyRate_1, HourlyRate.Median = HourlyRate_2, HourlyRate.Mean = HourlyRate_3, HourlyRate.Max = HourlyRate_4)    }
-  })
+#  output$sum2 <- renderTable({
+   # if(input$plotType1 == "p"){
+    #  employee_data3 %>% 
+     #   select(Age, HourlyRate) %>% 
+      #  summarise(across(everything(), list(min, median, mean, max))) %>%
+    #    rename(Age.Min = Age_1, Age.Median = Age_2 , Age.Mean = Age_3 , Age.Max = Age_4, 
+    #           HourlyRate.Min = HourlyRate_1, HourlyRate.Median = HourlyRate_2, HourlyRate.Mean = HourlyRate_3, HourlyRate.Max = HourlyRate_4)
+   # } else {
+    #  employee_data3 %>% 
+      #  select(Age, HourlyRate) %>% 
+     #   summarise(across(everything(), list(min, median, mean, max))) %>%
+      #  rename(Age.Min = Age_1, Age.Median = Age_2 , Age.Mean = Age_3 , Age.Max = Age_4, 
+         #      HourlyRate.Min = HourlyRate_1, HourlyRate.Median = HourlyRate_2, HourlyRate.Mean = HourlyRate_3, HourlyRate.Max = HourlyRate_4)    }
+ # })
   
   #code above is for tab 2
   
-  output$plot3 <- renderPlot({
-    if(input$plotType3 == "box"){
-      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
-        geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
-        labs(x = "Business Travel", 
-             y = "Job Satisfaction",
-             title = "Satisfaction Level in Regards to Travel",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    } else {
-      ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
-        geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
-        labs(x = "Business Travel", 
-             y = "Job Satisfaction",
-             title = "Satisfaction Level in Regards to Travel",
-             caption = "Source: IBM HR Analytics") +
-        theme(plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5))
-    }
-  })
-output$sum3 <- renderTable({
-  if(input$plotType3 == "box"){
-    employee_data3 %>% 
-      group_by(BusinessTravel) %>% 
-      summarise(JobSatisfaction.min = min(JobSatisfaction), JobSatisfaction.median = median(JobSatisfaction), JobSatisfaction.mean = mean(JobSatisfaction), JobSatisfaction.max = max(JobSatisfaction))
-  } else {
-    employee_data3 %>% 
-      group_by(BusinessTravel) %>% 
-      summarise(JobSatisfaction.min = min(JobSatisfaction), JobSatisfaction.median = median(JobSatisfaction), JobSatisfaction.mean = mean(JobSatisfaction), JobSatisfaction.max = max(JobSatisfaction))
-}
-})
-  }
+  #output$plot3 <- renderPlot({
+    #if(input$plotType3 == "box"){
+    #  ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+     #   geom_boxplot(fill = wes_palette("GrandBudapest2", n = 3)) +
+     #   labs(x = "Business Travel", 
+        #     y = "Job Satisfaction",
+           #  title = "Satisfaction Level in Regards to Travel",
+         #    caption = "Source: IBM HR Analytics") +
+        #theme(plot.title = element_text(hjust = 0.5),
+        #      plot.subtitle = element_text(hjust = 0.5))
+    #} else {
+    #  ggplot(employee_data3, aes(x = BusinessTravel, y = JobSatisfaction)) +
+     #   #geom_violin(fill = wes_palette("GrandBudapest1", n = 1)) +
+        #labs(x = "Business Travel", 
+           #  y = "Job Satisfaction",
+            # title = "Satisfaction Level in Regards to Travel",
+            # caption = "Source: IBM HR Analytics") +
+      #  theme(plot.title = element_text(hjust = 0.5),
+            #  plot.subtitle = element_text(hjust = 0.5))
+   # }
+ # })
+#output$sum3 <- renderTable({
+ # if(input$plotType3 == "box"){
+   # employee_data3 %>% 
+     # group_by(BusinessTravel) %>% 
+     # summarise(JobSatisfaction.min = min(JobSatisfaction), JobSatisfaction.median = median(JobSatisfaction), JobSatisfaction.mean = mean(JobSatisfaction), JobSatisfaction.max = max(JobSatisfaction))
+ # } else {
+  #  employee_data3 %>% 
+     # group_by(BusinessTravel) %>% 
+      #summarise(JobSatisfaction.min = min(JobSatisfaction), JobSatisfaction.median = median(JobSatisfaction), JobSatisfaction.mean = mean(JobSatisfaction), JobSatisfaction.max = max(JobSatisfaction))
+#}
+#})
+ # }
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 
 #--------------------------------------------------------------------------------------------------------------------------------
